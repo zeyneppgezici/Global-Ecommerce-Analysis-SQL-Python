a6d38cb1998e02c410c36a7a227e6c4d75a7109d
zeyneppgezici <zgezici98@gmail.com>
2026-03-01 21:45:26 +0300

    Update 01_create_table_and_import.sql

--- 01_create_table_and_import.sql
+++ 01_create_table_and_import.sql
@@ -1,10 +1,10 @@
 CREATE TABLE ecommerce_sales (
-    order_id INT,
+    order_id INT NOT NULL PRIMARY KEY,
     order_date DATE,
     country VARCHAR(50),
     product_category VARCHAR(50),
     unit_price DECIMAL(10,2),
     quantity INT,
     total_amount DECIMAL(10,2),
-    customer_id INT
+    
 );
